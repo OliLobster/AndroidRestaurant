606be1019b7f5a85a4c84a44883697982aeb47c4
OliLobster <sgyr1993@163.com>
2022-09-10 03:10:52 +0800

    enable adding Fragments dynamically

--- app/src/main/java/com/ollie/androidrestaurant/MainActivity.java
+++ app/src/main/java/com/ollie/androidrestaurant/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ollie.androidrestaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -21,5 +23,18 @@ public class MainActivity extends AppCompatActivity {
         RestaurantAdapter adapter = new RestaurantAdapter(this);
 
         eventListView.setAdapter(adapter);*/
+        // Show different fragments based on screen size.
+        if (findViewById(R.id.fragment_container) != null) {
+            Fragment fragment = isTablet() ? new RestaurantGridFragment()
+                    : new RestaurantListFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,
+                    fragment).commit();
+        }
+    }
+
+    private boolean isTablet() {
+        return (getApplicationContext().getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) >=
+                Configuration.SCREENLAYOUT_SIZE_LARGE;
     }
 }
